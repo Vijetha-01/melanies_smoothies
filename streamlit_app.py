59f33f271942ce0bb5d450e660998b09786f87a1
Vijetha-01 <vijetha.k.s@accenture.com>
2025-02-06 10:37:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,12 @@ if ingredients_list:
          #st.write(ingredients_string) 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
        values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-   
-
-   #st.write(my_insert_stmt)
-  # st.stop()
-
-   #st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
     time_to_insert =  st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
-     if time_to_insert:
-         session.sql(my_insert_stmt).collect()
-         st.success('Your Smoothie is ordered!', icon="✅")
-      
  
     
 
